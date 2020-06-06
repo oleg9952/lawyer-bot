20cde851e5ce72c5d1aadc9f3d2b58d00d6bfd11
Oleg <oleg.parhomenko2599@gmail.com>
2020-06-06 16:59:28 +0300

    animation state moved to redux

--- client/src/Redux/actions/index.js
+++ client/src/Redux/actions/index.js
@@ -6,3 +6,12 @@ export const botOneActions = {
     handleBack: 'HANDLE_BACK',
     handleReset: 'HANDLE_RESET'
 }
+
+export const homeActions = {
+    // ----- SECTION_ONE -----
+    toggleOneActive: 'TOGGLE_ONE_ACTIVE',
+    setOneVisited: 'SET_ONE_VISITED',
+    // ----- SECTION_TWO -----
+    toggleTwoActive: 'TOGGLE_TWO_ACTIVE',
+    setTwoVisited: 'SET_TWO_VISITED'
+}
--- client/src/Redux/reducers/index.js
+++ client/src/Redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import botOneReducer from './botOneReducer'
+import homeReducer from './homeReducers'
 
 export default combineReducers({
-    botOneReducer
+    botOneReducer,
+    homeReducer
 })
